0327b03bd06ed1bbed6e4c08c51fde072b242139
Satyam-kumar7079 <68579591+Satyam-kumar7079@users.noreply.github.com>
2020-08-10 12:45:45 +0530

    Flutter be integrate with Docker..

--- main.dart
+++ main.dart
@@ -1,6 +1,59 @@
- import 'package:flutter/material.dart';
- import 'ui/home.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-main() {
-   runApp(myapp());
+void main() {
+   runApp(MyTechApp());
  }
+
+ web() async {
+   var url = "http://192.168.43.14";
+   var reponse = await http.get(url);
+   print(reponse.body);
+ }
+
+ // ignore: must_be_immutable
+ class  MyTechApp extends StatelessWidget {
+   String cmd;
+   
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+       debugShowCheckedModeBanner: false,
+       home: Scaffold(
+         appBar: AppBar(
+           title: Text('Satyam App'),
+           ),
+           body: Center(
+             child: Container(
+               width: 300,
+               height: 300,
+               color: Colors.blueGrey.shade300,
+               child: Column(
+                 children: <Widget>[
+                   TextField(
+                     onChanged: (value) {
+                       cmd = value;
+                     },
+                     autocorrect: false,
+                     textAlign: TextAlign.center,
+                     decoration: InputDecoration(
+                       border: OutlineInputBorder(),
+                       hintText: "Enter ur CMD",
+                       prefixIcon: Icon(Icons.tablet_android),
+                     ),
+                   ),
+                   FlatButton(
+                     onPressed: () {
+                       web();
+                       print(cmd);
+                     },
+                    child: Text('click here')),
+                   //Text('sec'),
+                 ],),
+             ),
+           ),
+           ),
+       
+     );
+   }
+ } 
