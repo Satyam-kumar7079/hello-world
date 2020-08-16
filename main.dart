00d2c8718ad215e5ae583e3b3e960508422c85e3
Satyam-kumar7079 <68579591+Satyam-kumar7079@users.noreply.github.com>
2020-08-16 14:02:17 +0530

    Add files via upload
    Integrate flutter with docker and use any command of Linux in it...

--- main.dart
+++ main.dart
@@ -2,58 +2,80 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
-   runApp(MyTechApp());
+   runApp(MaterialApp(
+     debugShowCheckedModeBanner: false,
+     home: MyApp(),
+   ),
+   );
  }
 
- web() async {
-   var url = "http://192.168.43.14";
-   var reponse = await http.get(url);
-   print(reponse.body);
- }
-
- // ignore: must_be_immutable
- class  MyTechApp extends StatelessWidget {
-   String cmd;
+class MyApp extends StatefulWidget {
+   @override
+   _MyAppState createState() => _MyAppState();
+ } 
+ class _MyAppState extends State<MyApp> {
+   var cmd;
+   var webdata;
    
+   myweb(cmd) async {
+     // ignore: unnecessary_brace_in_string_interps
+     var url = "http://192.168.43.14/cgi-bin/sample.py?x=${cmd}";
+     var r = await http.get(url);
+
+     setState(() {
+       webdata = r.body;
+       });
+    // print(webdata);
+   }
+   /*var db = ["satyam", "rahul","saurbh","guddu"];
+   var index = 0;
+   davclick() {
+     setState(() {
+        });
+     index += 1;
+     print("Clicked!!!!");
+     print("Index Change!!!");
+     print(index);
+
+   }
+   */
    @override
    Widget build(BuildContext context) {
-     return MaterialApp(
-       debugShowCheckedModeBanner: false,
-       home: Scaffold(
+     return Scaffold(
          appBar: AppBar(
-           title: Text('Satyam App'),
-           ),
-           body: Center(
-             child: Container(
-               width: 300,
-               height: 300,
-               color: Colors.blueGrey.shade300,
-               child: Column(
-                 children: <Widget>[
-                   TextField(
-                     onChanged: (value) {
-                       cmd = value;
-                     },
-                     autocorrect: false,
-                     textAlign: TextAlign.center,
-                     decoration: InputDecoration(
-                       border: OutlineInputBorder(),
-                       hintText: "Enter ur CMD",
-                       prefixIcon: Icon(Icons.tablet_android),
-                     ),
-                   ),
-                   FlatButton(
-                     onPressed: () {
-                       web();
-                       print(cmd);
-                     },
-                    child: Text('click here')),
-                   //Text('sec'),
-                 ],),
-             ),
-           ),
-           ),
+         title: Text('TCS'),
+       ),
+       body: Center(
+       child: Container(
+         width: MediaQuery.of(context).size.width * 0.8,
+         height: MediaQuery.of(context).size.height * 0.8,
+         color: Colors.grey,
+         child: Column(
+           children: <Widget>[
+          Text("Enter ur linux cmd ::"),
+          TextField(onChanged: (value) {
+            cmd = value;
+          },
+          autocorrect: false,
+          textAlign: TextAlign.center,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+          ),
+          ),
+          // ignore: missing_required_param
+          FlatButton(
+            onPressed: () {
+              myweb(cmd);
+          },
+           child: Text('click here')),
+          Text(webdata ?? "Output Coming..."),
+          ],
+         ),
+
+         )
+
+       )
        
      );
    }
- } 
+ }
