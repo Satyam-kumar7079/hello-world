7b75123dafbd4641efbb3f1216536541b2adc0b5
Satyam Kumar <68579591+Satyam-kumar7079@users.noreply.github.com>
2020-08-19 01:58:07 +0530

    Add files via upload
    In this project we can check ur current location.

--- main.dart
+++ main.dart
@@ -1,81 +1,55 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+
 
 void main() {
    runApp(MaterialApp(
      debugShowCheckedModeBanner: false,
-     home: MyApp(),
-   ),
-   );
+     home: MyGoggleMap(),
+     theme: ThemeData.dark(),
+     ));
  }
+ 
+class MyGoggleMap extends StatefulWidget {
+  @override
+  _MyGoggleMapState createState() => _MyGoggleMapState();
+}
 
-class MyApp extends StatefulWidget {
-   @override
-   _MyAppState createState() => _MyAppState();
- } 
- class _MyAppState extends State<MyApp> {
-   var cmd;
-   var webdata;
-   
-   myweb(cmd) async {
-     // ignore: unnecessary_brace_in_string_interps
-     var url = "http://192.168.43.14/cgi-bin/sample.py?x=${cmd}";
-     var r = await http.get(url);
+class _MyGoggleMapState extends State<MyGoggleMap> {
+  var lat1 = 23.013278;
+  var long1 = 84.609366;
 
-     setState(() {
-       webdata = r.body;
-       });
-    // print(webdata);
-   }
-   /*var db = ["satyam", "rahul","saurbh","guddu"];
-   var index = 0;
-   davclick() {
-     setState(() {
-        });
-     index += 1;
-     print("Clicked!!!!");
-     print("Index Change!!!");
-     print(index);
-
-   }
-   */
-   @override
-   Widget build(BuildContext context) {
-     return Scaffold(
-         appBar: AppBar(
-         title: Text('TCS'),
-       ),
-       body: Center(
-       child: Container(
-         width: MediaQuery.of(context).size.width * 0.8,
-         height: MediaQuery.of(context).size.height * 0.8,
-         color: Colors.grey,
-         child: Column(
-           children: <Widget>[
-          Text("Enter ur linux cmd ::"),
-          TextField(onChanged: (value) {
-            cmd = value;
-          },
-          autocorrect: false,
-          textAlign: TextAlign.center,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
+  // ignore: non_constant_identifier_names
+  Press() async {
+     var p = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    lat1 = p.latitude;
+    long1 = p.longitude;
+    print(p);
+    }
+     @override
+      Widget build(BuildContext context) {
+        var cp = CameraPosition(
+          target: LatLng(lat1,long1),
+          zoom: 19,
+          tilt: 50,
+           );
+    
+        return Scaffold(
+          appBar: AppBar(
+          title: Text('Goggle Map'),
           ),
+          body: GoogleMap(
+            initialCameraPosition: cp,
+            mapType: MapType.hybrid,
           ),
-          // ignore: missing_required_param
-          FlatButton(
-            onPressed: () {
-              myweb(cmd);
-          },
-           child: Text('click here')),
-          Text(webdata ?? "Output Coming..."),
-          ],
-         ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: Press,
+            child: Icon(Icons.add),
+          ),
+              );
+      }
+    }
+    
 
-         )
-
-       )
-       
-     );
-   }
- }
